fe4bf2f5e832b11bccd17a575f4a3251bc73cd81
johnverz <johnverz77@gmail.com>
2025-01-16 11:55:47 +0800

    Made an update to Main.java

--- src/main/java/com/johnverz/Main.java
+++ src/main/java/com/johnverz/Main.java
@@ -1,20 +1,67 @@
 package com.johnverz;
 
-import org.mindrot.jbcrypt.BCrypt;
+import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        String password = "Ilovejava";
-        String hashed = BCrypt.hashpw(password, BCrypt.gensalt(12));
-        System.out.println(hashed);
+/**
+ * Student Grade Calculator
+ * Lets the user enter his prelim,
+ * midterm, final grades
+ * and the program will compute the
+ * final rating and save the grades into file
 
-        String loginPassword = "Ilovejav";
-        if(BCrypt.checkpw(loginPassword, hashed)){
-            System.out.println("You are authorized!");
-        }else{
-            System.out.println("Are you a hacker?");
+ */
+public class Main{
+    public static final double MIN_GRADE = 50;
+    public static void main(String[] args){
+        //datatype varName = initialValue;
+        String name;
+        String subject;
+        double[] grades = new double[3];
+        /*
+        grades[0] = prelim
+        grades[1] = midterm
+        grades[2] = finals
+         */
+
+        String[] terms = {"Prelim", "Midterm", "Finals"};
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter name: ");
+        name = scanner.nextLine();
+
+        System.out.print("Enter subject: ");
+        subject = scanner.nextLine();
+
+
+        for(int i = 0; i < grades.length; i++){
+            System.out.print("Enter grade for " + terms[i] + ": ");
+            grades[i] = scanner.nextInt();
+            if(grades[i] < MIN_GRADE){
+                System.out.println("Invalid grade");
+                i--;
+            }
+
         }
+
+        String result = String.format("""
+                Name: %s
+                Subject: %s
+                Prelim: %.2f
+                Midterm: %.2f
+                Finals: %.2f
+                Final Rating: %.2f
+                """, name, subject, grades[0], grades[1], grades[2], getFinalRating(grades));
+
+        System.out.println("Grade Computation Results");
+        System.out.println(result);
+
+
     }
+
+    public static double getFinalRating(double[] termGrades){
+        double finalRating = termGrades[0] * .3 + termGrades[1] * .3 + termGrades[2] * .4;
+        return finalRating;
+    }
+
 }
