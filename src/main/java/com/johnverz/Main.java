372c9a67b245fc9abf903cd05a6f564bb76a0d36
johnverz22 <johnny.verzola@lorma.edu>
2025-02-10 09:24:59 +0800

    introduced encapsulation

--- src/main/java/com/johnverz/Main.java
+++ src/main/java/com/johnverz/Main.java
@@ -1,144 +1,39 @@
 package com.johnverz;
 
-import java.io.*;
-import java.net.URL;
-import java.util.Scanner;
 
-/**
- * Student Grade Calculator
- * Lets the user enter his prelim,
- * midterm, final grades
- * and the program will compute the
- * final rating and save the grades into file
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
- */
 public class Main{
-    public static final double MIN_GRADE = 50;
-    public static final String FILE_DIR = "target/records";
-    public static final int NUM_OF_SUBJECTS = 64;
-    public static final int NUM_OF_TERMS = 3;
-    public static void main(String[] args){
-        //datatype varName = initialValue;
-        String name;
-        String subject;
+    public static void main(String[] args) {
+        Student student1 = new Student(); //creating an object -> instantiating an object Student
+        student1.setId(240414);
 
-        String[] subjects = new String[NUM_OF_SUBJECTS];
-        /*
-            DSA = array, array list, list, linked list
-        */
+        System.out.println(student1.getId());
+        student1.lastName = "Ciubal";
+        student1.firstName = "Regin Jacob";
+        student1.middleName = "Heruela";
+        student1.email = "reginjacob.ciubal@lorma.edu";
+        student1.birthDay = LocalDate.parse("2005-07-12", DateTimeFormatter.ofPattern("yyyy-MM-dd"));
 
-        //double[] grades = new double[3];
-        double[][] grades = new double[NUM_OF_SUBJECTS][NUM_OF_SUBJECTS];
-
-        /*
-        grades[0] = prelim
-        grades[1] = midterm
-        grades[2] = finals
-         */
-
-        String[] terms = {"Prelim", "Midterm", "Finals"};
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter name: ");
-        name = scanner.nextLine();
-
-        StringBuilder sb = new StringBuilder();
-        sb.append("Name: ").append(name);
-        // String sb = "Name: " + name;
-        //HEADER
-        sb.append("\n").append(String.format(
-                        "%-15s%-10s%-10s%-10s%s\n",
-                        "SUBJECTS","PRELIM", "MIDTERM", "FINAL","FINAL RATING"
-                )
-        );
+        System.out.println(student1.birthDay.getYear());
+        Period age = Period.between(LocalDate.now(), student1.birthDay);
+        int a = age.getYears();
+        System.out.println("Age: " + Math.abs(a));
 
 
-        for(int i = 0; i < NUM_OF_SUBJECTS; i++){
-            System.out.print("Enter subject: ");
-            subjects[i] = scanner.nextLine();
-            sb.append(String.format("%-15s", subjects[i]));
+        LocalDate s2Bday = LocalDate.parse("2004-11-16", DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        Student s2 = new Student(2402497,
+                "Nera",
+                "Christian Jacob",
+                "Torres",
+                s2Bday,
+                "christianjacob.nera@lorma.edu");// creating an object
 
-            for(int j = 0; j < NUM_OF_TERMS; j++){
-                System.out.print("\t" + terms[j] + ": ");
-                try {
-                    grades[i][j] = Double.parseDouble(scanner.nextLine());
-                    if(grades[i][j] < MIN_GRADE){
-                        throw new Exception("Error! No grades lesser than 50.");
-                    }
-                } catch (NumberFormatException e) {
-                    System.out.println("\tError! Invalid number.");
-                    --j;
-                    continue;
-                } catch(Exception e){
-                    System.out.println("\t" + e.getMessage());
-                    --j;
-                    continue;
-                }
+        System.out.println(s2.getFullName());
+        System.out.println(s2.getAge());
 
-                sb.append(String.format("%-10.2f", grades[i][j]));
-
-            }
-
-            System.out.print("Add another subject (y/n): ");
-            char choice = scanner.nextLine().charAt(0);
-
-            sb.append(String.format("%.2f\n", getFinalRating(grades[i])));
-
-            if(Character.toLowerCase(choice) != 'y')
-                break;
-        }
-
-        //PRINT RESULT
-        System.out.println(sb);
-        writeToFile(name, sb.toString());
     }
-
-    /**
-     * This fuction computes for the final rating
-     * Formula: 30% of Prelim + 30 % of Midterm + 40% of Finals
-     *
-     * @param termGrades double[] - of 3 elements containing grade for prelims, midterm and finals
-     * @return - double - the final rating
-     */
-    public static double getFinalRating(double[] termGrades){
-        double finalRating = termGrades[0] * .3 + termGrades[1] * .3 + termGrades[2] * .4;
-        return finalRating;
-    }
-
-
-    public static void writeToFile(String fileName,  String data){
-        File folder = new File(FILE_DIR);
-        if(folder.exists() == false){
-            folder.mkdirs();
-        }
-        File file = new File(folder, fileName);
-        try ( FileWriter fw = new FileWriter(file)) {
-            fw.write(data);
-        } catch (IOException e) {
-            System.out.println("Error saving file: " + e.getMessage());
-        }
-    }
-    public static void readAllFiles(){
-        File folder = new File(FILE_DIR);
-        File[] studentGradeFiles = folder.listFiles();
-        for(int i = 0; i < studentGradeFiles.length; i++){
-            if(studentGradeFiles[i].isFile()){
-                readFile(studentGradeFiles[i]);
-            }
-        }
-    }
-
-    public static void readFile(File file){
-        try(BufferedReader br = new BufferedReader(new FileReader(file))){
-            System.out.println("--------------------------");
-            String line;
-            while((line = br.readLine()) !=null ){
-                System.out.println(line);
-            }
-        }catch (IOException e){
-            System.out.println("Error: " + e.getMessage());
-        }
-    }
-
 }
--- src/main/java/com/johnverz/Student.java
+++ src/main/java/com/johnverz/Student.java
@@ -1,4 +1,73 @@
 package com.johnverz;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Date;
+
 public class Student {
+    private int id;
+    private String lastName;
+    private String firstName;
+    private String middleName;
+    private LocalDate birthDay;
+    private String email;
+
+    public Student(){
+
+    }
+
+    /**
+     *
+     * @param id Student ID
+     * @param lastName
+     * @param firstName
+     * @param middleName
+     * @param birthDay
+     * @param email
+     */
+    public Student(int id,
+                   String lastName,
+                   String firstName,
+                   String middleName,
+                   LocalDate birthDay,
+                   String email){
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.middleName = middleName;
+        this.birthDay = birthDay;
+        this.email = email;
+    }
+
+    public String getFullName(){
+        return firstName + " " + lastName;
+    }
+
+    public int getAge(){
+        Period age = Period.between(this.birthDay, LocalDate.now());
+        return age.getYears();
+    }
+
+    public void setId(int id){
+        this.id=id;
+    }
+    public int getId(){
+        return id;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
