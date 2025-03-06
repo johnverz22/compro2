6bbf2ae2bae26f291cb19d3cc08a6ddd75c2205d
johnverz22 <johnny.verzola@lorma.edu>
2025-03-06 08:47:50 +0800

    created first branch

---
+++ src/test/java/com/johnverz/student/StudentG2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johnverz.student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentG2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
